20e7c40d3a2da719e97217d174a831f69e1731ac
Artem <noskov.artem@gmail.com>
2021-02-16 12:50:58 +0300

    homework 4

--- src/main/java/homework4/Main.java
+++ src/main/java/homework4/Main.java
@@ -1,4 +1,55 @@
 package homework4;
 
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String[] arrayWords = new String[]{"potato", "cucumber", "tomato", "onion", "garlic", "carrot", "onion", "garlic", "mushrooms", "tomato", "tomato", "cucumber", "carrot", "pepper", "cabbage", "pumpkin", "squash", "pepper", "pumpkin", "cucumber"};
+        ArrayList listWords = new ArrayList<>(Arrays.asList(arrayWords));
+
+        Set<String> unique = new HashSet<>();
+
+        Iterator<String> iteration = listWords.iterator();
+        while (iteration.hasNext()) {
+            String s = iteration.next();
+            unique.add(s);
+        }
+
+        System.out.println("ex.1");
+        System.out.println();
+        System.out.println("Unique values:");
+        System.out.println(unique);
+        System.out.println();
+
+        HashMap<String, Integer> countWords = new HashMap<>();
+        System.out.println("Duplicate values:");
+        for (String word : arrayWords) {
+            if (!countWords.containsKey(word)) {
+                countWords.put(word, 0);
+            }
+
+            countWords.put(word, countWords.get(word) + 1);
+        }
+
+        for (String word : countWords.keySet()) {
+            System.out.println(word + " " + countWords.get(word));
+        }
+
+        System.out.println();
+        System.out.println("ex. 2");
+        System.out.println();
+
+        Telephone.addDefault();
+        Telephone.add("Kukushkin 96959");
+        Telephone.listAll();
+        Telephone.get("Abromovich");
+        Telephone.get("Usmanov");
+        Telephone.get("Pupkin");
+        Telephone.get("Kotov");
+
+    }
+
 }
+
--- src/main/java/homework4/Telephone.java
+++ src/main/java/homework4/Telephone.java
@@ -1,4 +1,48 @@
 package homework4;
 
+import java.util.ArrayList;
+
 public class Telephone {
+
+    public static ArrayList<String> arListPhone = new ArrayList<>();
+
+    public static void addDefault() {
+        arListPhone.add("Ivanov 11223");
+        arListPhone.add("Petrov 21212");
+        arListPhone.add("Sidorov 32323");
+        arListPhone.add("Pupkin 65956");
+        arListPhone.add("Smirnov 48796");
+        arListPhone.add("Kuznetcov 54545");
+        arListPhone.add("Abromovich 89852");
+        arListPhone.add("Usmanov 77777");
+        arListPhone.add("Volkov 85696");
+    }
+
+    public static void add(String arListPhone) {
+        Telephone.arListPhone.add(arListPhone);
+    }
+
+    public static void listAll() {
+        System.out.println("Phonebook:");
+        for (int i = 0; i < arListPhone.size(); i++) {
+            System.out.println(arListPhone.get(i));
+        }
+    }
+
+
+    public static void get(String surname) {
+        boolean res = false;
+        System.out.println();
+        System.out.println("Search name \"" + surname + "\": ");
+        for (int i = 0; i < arListPhone.size(); i++) {
+            if (arListPhone.get(i).lastIndexOf(surname) != -1) {
+                System.out.println(arListPhone.get(i));
+                res = true;
+            }
+        }
+        if (!res) {
+            System.out.println("Not found!");
+        }
+
+    }
 }
